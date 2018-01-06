1c58d21fc5de3c3e78fd95d062b1c3e8cf064909
nguyenvannam19942802 <nguyenvannam19942802@gmail.com>
2018-01-06 11:30:07 +0700

    #Add new   - Viết ứng dụng timer app

---
+++ app/src/main/java/com/app/timer/timerapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.app.timer.timerapp;
+
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button startBtn;
+    public Button pauseBtn;
+    public TextView timerValue;
+    public long startTime = 0L;
+    public Handler customHandler = new Handler();
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerValue = (TextView) findViewById(R.id.timerValue);
+        startBtn = (Button) findViewById(R.id.startBtn);
+        pauseBtn = (Button) findViewById(R.id.pauseBtn);
+
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            timerValue.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+
+            customHandler.postDelayed(this, 0);
+
+        }
+    };
+}
